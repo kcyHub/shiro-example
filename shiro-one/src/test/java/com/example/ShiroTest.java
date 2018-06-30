f928715b32dd9e001131fbbcc479a78d619746b3
kcyHub <Kcy@1236547>
2018-06-30 12:13:07 +0800

    commit

---
+++ shiro-one/src/test/java/com/example/ShiroTest.java
@@ -0,0 +1,48 @@
+package com.example;
+
+import org.apache.shiro.SecurityUtils;
+import org.apache.shiro.authc.AuthenticationException;
+import org.apache.shiro.authc.UsernamePasswordToken;
+import org.apache.shiro.config.IniSecurityManagerFactory;
+import org.apache.shiro.mgt.SecurityManager;
+import org.apache.shiro.subject.PrincipalCollection;
+import org.apache.shiro.subject.Subject;
+import org.apache.shiro.util.Factory;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ShiroTest {
+
+    @Test
+    public void testLogin() {
+//        String configFile = "shiro-.ini";
+//        String configFile = "shiro-realm.ini";
+        String configFile = "shiro-jdbc-realm.ini";
+
+        // 1、获取SecurityManager工厂，此处使用Ini配置文件初始化SecurityManager
+        Factory<SecurityManager> factory = new IniSecurityManagerFactory(configFile);
+        // 2、得到SecurityManager实例 并绑定给SecurityUtils
+        SecurityManager securityManager = factory.getInstance();
+        SecurityUtils.setSecurityManager(securityManager);
+        // 3、得到Subject及创建用户名/密码身份验证Token（即用户身份/凭证）
+        Subject subject = SecurityUtils.getSubject();
+
+        UsernamePasswordToken token = new UsernamePasswordToken("wang", "123");
+//        UsernamePasswordToken token = new UsernamePasswordToken("zhang", "1234");
+
+        try {
+            // 4、登录，即身份验证
+            subject.login(token);
+        } catch (AuthenticationException e) {
+            // 5、身份验证失败
+            e.printStackTrace();
+        }
+
+        // 断言用户已经登录
+        assertTrue(subject.isAuthenticated());
+
+        // 退出
+        subject.logout();
+    }
+}
---
+++ shiro-one/src/test/java/com/example/realm/MyRealm1.java
@@ -0,0 +1,35 @@
+package com.example.realm;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.realm.Realm;
+
+public class MyRealm1 implements Realm {
+
+    @Override
+    public String getName() {
+        return "myrealm1";
+    }
+
+    @Override
+    public boolean supports(AuthenticationToken authenticationToken) {
+        //仅支持UsernamePasswordToken类型的Token
+        return authenticationToken instanceof UsernamePasswordToken;
+    }
+
+    @Override
+    public AuthenticationInfo getAuthenticationInfo(AuthenticationToken authenticationToken) throws AuthenticationException {
+        // 得到用户名和密码
+        String username = (String) authenticationToken.getPrincipal();
+        String password = new String((char[]) authenticationToken.getCredentials());
+
+        if (!"zhang".equals(username)) {
+            throw new UnknownAccountException(); // 如果用户名错误
+        }
+
+        if (!"123".equals(password)) {
+            throw new IncorrectCredentialsException(); // 如果密码错误
+        }
+
+        return new SimpleAuthenticationInfo(username, password, getName());
+    }
+}
